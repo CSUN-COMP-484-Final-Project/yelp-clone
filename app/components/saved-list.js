a0eda939b49b0facec359ddf782de4fe2404be36
Jan Navarro <navarrojanderrick@gmail.com>
2021-12-14 22:23:41 -0800

    Add Saved for Later component

--- app/components/saved-list.js
+++ app/components/saved-list.js
@@ -1,3 +1,22 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
-export default class SavedListComponent extends Component {}
+export default class SavedListComponent extends Component {
+  @service store;
+
+  constructor(...args) {
+    super(...args);
+  }
+
+  favorite = (business) => {
+    this.store.favoriteBusiness(business);
+    console.log('Favorited', business);
+  };
+
+  remove = (index) => {
+    const temp = [...this.store.saved];
+    const [removed] = temp.splice(index, 1);
+    this.store.setSaved(temp);
+    console.log('Removed', removed);
+  };
+}
